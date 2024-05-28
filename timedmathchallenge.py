5555bc1f11f01c9053f77a08a0aa645fa2b79f26
dyinks <92653275+dyinks@users.noreply.github.com>
2024-05-28 15:07:02 -0700

    Add files via upload

---
+++ timedmathchallenge.py
@@ -0,0 +1,34 @@
+import random
+import time
+
+MAX_VAL = 12
+MIN_VAL = 1
+OPERATORS = ["+", "-", "*"]
+TOTAL_PROB = 5
+
+def gen_prob():
+    left = random.randint(MIN_VAL, MAX_VAL)
+    right = random.randint(MIN_VAL, MAX_VAL)
+    operator = random.choice(OPERATORS)
+
+    expr = str(left) + " " + operator + " " + str(right)
+    answer = eval(expr)
+    return expr, answer
+
+score = 0
+print("Press Enter to start")
+print("-----------------------")
+
+star_time = time.time()
+for i in range(TOTAL_PROB):
+    expr, answer = gen_prob()
+    while True:
+        guess = input("problem #" + str(i+1) + ": " + expr + " = ")
+        if guess == str(answer):
+            break
+        score += 1
+end_time = time.time()
+total_time = end_time - star_time
+
+print("-----------------------")
+print(f"You finished in {round(total_time)} seconds ")
